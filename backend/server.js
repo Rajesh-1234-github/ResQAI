3e8b9d4414694375a93e4522d92250ba08794486
RAJESH CHOWDUVADA <rajeshchowduvada123@gmail.com>
2026-06-06 12:08:09 +0530

    Updated MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,10 @@ app.use(express.json())
 // Static Folder for Uploaded Files
 app.use("/uploads", express.static("uploads"))
 
+// MongoDB Atlas Connection
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Atlas Connected"))
-.catch((err) => console.log(err))
+  .then(() => console.log("MongoDB Atlas Connected"))
+  .catch((err) => console.log(err))
 
 app.use("/api", reportRoutes)
 
@@ -23,7 +24,8 @@ app.get("/", (req, res) => {
   res.send("ResQAI Backend Running Successfully")
 })
 
-const PORT = 5000
+// Render provides PORT automatically
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
